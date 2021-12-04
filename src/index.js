7ac5be0f3d0babd7cdf9bc5af36e7bbe6ca290c7
BatterB <dima.elizarov.2003@gmail.com>
2021-12-04 03:02:31 +0300

    любовь божественному противоестествена, а вот доделаный самое то

--- src/index.js
+++ src/index.js
@@ -15,4 +15,55 @@ navigation.registerTabs([
     {name:'main', component: main },
     {name:'hstr', component: hstr },
     {name:'Arc', component: Arc },
+    {name:'x86', component: x86 },
+    {name:'AMD', component: AMD },
+    {name:'AMD-now', component: AMDnow },
+    {name:'Intel', component: Intel },
+    {name:'IntelNow', component: IntelNow }
 ])
+
+let menuButton = document.getElementById('menuButton')
+let nav = document.getElementById('nav-menu')
+let state = 0;
+
+let line = document.getElementsByClassName('line')
+
+menuButton.addEventListener("click",()=>{
+    
+    if(state === 0){
+        nav.classList.add('show-menu');
+        state= 1;
+        Array.from(line).forEach(element => {
+            element.style.display = 'none';
+        });
+        menuButton.classList.add('crossButton');
+
+    }
+    else{
+        nav.classList.remove('show-menu');
+        state =0;
+        Array.from(line).forEach(element => {
+            element.style.display = 'block';
+        });
+        menuButton.classList.remove('crossButton');
+    }
+}
+)
+
+let imageNum = 1;
+
+function slaider(){
+    let nowImage = document.getElementById('slaider-item'+imageNum);
+    imageNum+=1;
+    if(imageNum>4){
+        imageNum=1;
+    }
+    let newImage = document.getElementById('slaider-item'+imageNum);
+    nowImage.classList.add("hide");
+    newImage.classList.remove("hide");
+    newImage.classList.add("show-slaid");
+    nowImage.classList.remove("show-slaid");
+}
+
+
+setInterval(slaider,3000)
